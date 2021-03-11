b8fac40aec91d0ae7fd83ea0e5c3e8270c2f30f0
panantakorn <rew555-.-@hotmail.com>
2021-03-11 13:25:53 +0700

    update

---
+++ bookstore/db.php
@@ -0,0 +1,20 @@
+<?php 
+ define("hostname","localhost");
+ define("riw","riw");
+ define("password","keereelird2543");
+ define("dbname","bookstore");
+?>
+
+<?php
+$mysqli = new mysqli(hostname,riw,password,dbname);
+//Check connection
+    if($mysqli -> connect_errno){
+        echo "Faild to connect to MySQL: ".$mysqli->connect_error;
+        exit();
+    } 
+    else{
+
+     echo "Connect success.....";
+}
+$sqltxt = "";
+?>
